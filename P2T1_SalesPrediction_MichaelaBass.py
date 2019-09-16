4f979fe27a864df36b10328dc2d704ae70a55ead
P3ach3s <55394588+P3ach3s@users.noreply.github.com>
2019-09-16 11:50:50 -0400

    Add files via upload
    Sales Production assignment

---
+++ P2T1_SalesPrediction_MichaelaBass.py
@@ -0,0 +1,13 @@
+#this program will display my name, address, phone number and college major
+print("Michaela Bass")
+print("1860 Farrell Rd, Sanford NC 27330")
+print("936-645-0906")
+print("Programming and development, but also potential double major for Network Security")
+
+#Sales Prediction exercise
+
+
+annSales= float(input("What were the total annual sales?"))
+annProfit= annSales*0.23
+print("Your annual profit is:")
+print(format(annProfit, '.2f'))
